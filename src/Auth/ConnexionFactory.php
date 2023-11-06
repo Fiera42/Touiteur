180e76fb9030820c5e9315b8115f55d44e73a8b1
Fiera42 <anaigeon@gmx.fr>
2023-11-06 17:28:21 +0100

    Clean Auth & db connexion

--- src/Auth/ConnexionFactory.php
+++ src/Auth/ConnexionFactory.php
@@ -1,29 +1,35 @@
 <?php
 
-namespace touiteur\src\Auth;
+namespace iutnc\touiteur\auth;
+use PDO;
 
-class ConnexionFactory
-{
-    private static $tconfig;
-    private static $pdo;
+class ConnexionFactory {
 
-    static function  setConfig($file)
-    {
-        self::$tconfig = parse_ini_file($file);
+    public static ?PDO $db = null;
+    static array $config;
+
+    static function setConfig(String $file) : void {
+        self::$config = parse_ini_file($file);
     }
-    public static function makeconexion()
-    {
-        if (!isset(self::$pdo)) {
 
+    static function makeConnection() : void {
+        if (self::$db == null) {
+            $dsn = self::$config['driver'].
+                ':host='.self::$config['host'].
+            ';dbname='.self::$config['database'];
 
-            $driver = self::$tconfig['driver'];
-            $host = self::$tconfig['host'];
-            $database = self::$tconfig['database'];
-            $dsn = "$driver:$host;dbname=$database; charset=utf8";
-            $username = self::$tconfig['username'];
-            var_dump(self::$tconfig);
-            self::$pdo = new PDO($dsn, $username, "");
+            try {
+                self::$db = new PDO($dsn, self::$config['username'], self::$config['password'], [
+                    PDO::ATTR_PERSISTENT => true,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_EMULATE_PREPARES => false,
+                    PDO::ATTR_STRINGIFY_FETCHES => false
+                ]);
+            } catch (PDOException $e) {
+                die('Erreur : '.$e->getMessage());
+            }
+
+            self::$db->prepare("SET NAMES 'utf8'")->execute();
         }
-        return static:: $pdo;
     }
 }
