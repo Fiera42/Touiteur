8585e11337496890d222b0d8a81e75ba5fca7c35
Flyguss <crafteur55100-verdun@hotmail.com>
2023-11-07 09:47:55 +0100

    ajout des insert

--- src/User/User.php
+++ src/User/User.php
@@ -2,15 +2,28 @@
 
 namespace touiteur\User;
 
+
+use PDO;
+use touiteur\auth\ConnexionFactory;
+
 class User {
     private String $email;
     private String $passwd;
     private int $role;
 
+    private int $id;
+
     function __construct(String $email, String $passwd, int $role = 0) {
         $this->email = $email;
         $this->passwd = $passwd;
         $this->role = $role;
+        ConnexionFactory::makeConnection();
+        $query = "select idUser from TouiteurUser where email=? and password = ?";
+        $prepared_query = ConnexionFactory::$db->prepare($query);
+        $prepared_query->bindParam(1, $this->email, PDO::PARAM_STR, 32);
+        $prepared_query->bindParam(2, $this->passwd, PDO::PARAM_STR, 32);
+        $prepared_query->execute();
+        $this->id = $prepared_query->fetchAll(PDO::FETCH_ASSOC)[0];
     }
 
     function getTouit() : array {
@@ -35,4 +48,8 @@ class User {
 
     }
 
+    function getId() : int
+    {
+
+    }
 }
