4bb1c20ebb3fc24354ae6f945306b8c283020a49
Flyguss <crafteur55100-verdun@hotmail.com>
2023-11-07 14:54:42 +0100

    loader

---
+++ src/Loader/Autoloader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace touiteur\loader;
+
+class Autoloader {
+
+    private string $prefixe;
+    private string $chemin;
+
+    public function __construct(string $prefixe, string $chemin) {
+        $this->prefixe = $prefixe;
+        $this->chemin = $chemin;
+    }
+
+    public function loadclass(string $classname) : void{
+        $classname = ltrim($classname);
+        $classname = rtrim($classname);
+
+        $filename = str_replace($this->prefixe, $this->chemin, $classname);
+        $filename = str_replace('\\', '/', $filename);
+        $filename = $filename.'.php';
+
+        if(is_file($filename)) require_once($filename) ;
+    }
+
+    public function register() : void {
+        spl_autoload_register([$this, 'loadClass']);
+    }
+}
