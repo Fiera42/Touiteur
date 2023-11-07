1bba0c2dcaeffa72a82e87c67210dbf385cc72cd
Kinnig Felicien <felicien.kinnig9@etu.univ-lorraine.fr>
2023-11-07 10:27:55 +0100

    removeTouite

--- src/User/TouiteList.php
+++ src/User/TouiteList.php
@@ -3,25 +3,27 @@
 namespace touiteur\TouiteList;
 
 class TouiteList{
-    private Array<Touite> $touite;
+    private Array<Touite> $touites;
     private int $nbTouitePerPage;
 
     public TouiteList(){
-        $this->touite = array();
+        $this->touites = array();
         $this->$nbTouitePerPage = 10;
     }
 
     public void addTouite(Touite $touite){
-        array_push($this->touite, $touite);
+        array_push($this->touites, $touite);
     }
 
     public void removeTouite(Touite $touite){
-        //TO DO
+        if (($key = array_search($touite, $this->touites)) !== false) {
+            unset($this->touites[$key]);
+        }
     }
 
     public String displayAllTouites(){
         string $res;
-        foreach($this->touite as $value){
+        foreach($this->touites as $value){
             $res += $value->displayTouiteSimple() . "/n" . "/n";
         }
         unset($value);
