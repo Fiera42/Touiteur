9ab1c09e5c5f20b1dbfcce9205aeef2bfc5924fd
unknown <fouquettom2004@gmail.com>
2023-11-08 15:02:14 +0100

    Action Vote

--- src/Action/ActionVote.php
+++ src/Action/ActionVote.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace touiteur\action;
+namespace touiteur\Action;
 
-class ActionPersonne extends Action {
+use touiteur\User\Touite;
+use touiteur\User\User;
+
+class ActionVote extends Action {
     public function execute() : string {
-        $_GET['idtouite']; //the id of the tag we want to look vote to
-        $_GET['value']; //True if positive vote, false if negative vote
+        $touite=Touite::getTouiteFromId($_GET['idtouite']); //the id of the tag we want to look vote to
+        $bool=$_GET['value']; //True if positive vote, false if negative vote
+        $user=User::getUserFromId($_SESSION['iduser']);
+        $touite->evaluateTouite($user,$bool);
         //make the changes of the db
         $html='Nothing';
         return $html;
