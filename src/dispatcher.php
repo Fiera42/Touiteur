0ef3d0bd4d68217b17d6226460e7c0d94169c08d
Fiera42 <anaigeon@gmx.fr>
2023-11-08 17:41:34 +0100

    dispatcher big touite

--- src/dispatcher.php
+++ src/dispatcher.php
@@ -91,55 +91,88 @@ class Dispatcher {
         if(isset($_SESSION['user'])) $profil = 'lookUser&iduser='.$_SESSION['user']->getId();
         else $profil = "register";
 
-        $html = "<!DOCTYPE html>
-        <html lang=\"fr\">
-           <head>
-              <title>Touiteur</title>
-              <meta charset=\"utf-8\">
-              <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-              <link rel=\"stylesheet\" href=\"css/mainStyle.css\">
-           </head>
-        
-           <body>
-            <header>
-                <img class=\"logo\" src=\"./ressource/Logo.png\" alt=\"Le logo de touiteur\">
-            </header>
-        
-            <nav class=\"nav\">
-                <a class=\"nav\" href=\"?\"><button class=\"stylized\">home</button></a>
-                <a class=\"nav\" href=\"?action={$profil}\"><button class=\"stylized\">profile</button></a>
-                <a class=\"nav\" href=\"?action=search\"><button class=\"stylized\">search</button></a>
-            </nav>
-        
-            <aside class=\"mascotte\">
-                <img src=\"https://media.tenor.com/BXQgJskV7LgAAAAj/9999.gif\">
-            </aside>
-        
-            <div id=\"feed\">
-                <!-- NOTE : only visible for connected user-->
-                <form class=\"post\" method=\"post\" action=\"?action=touite\" {$hidePost}>
-                    <h2>Envoyer un touite</h2>
-                    <textarea name=\"text\" maxlength=\"235\" placeholder=\"Votre incroyable touite ici\"> </textarea>
-                    <input type=\"file\" name=\"img\">
-                    <input type=\"text\" name=\"img_desc\" placeholder=\"Une description de votre image\">
-                    <button type=\"submit\">Envoyer le touite</button>
-                </form>
-        
-                <!-- when not connected, show that instead -->
-                <form class=\"connection\" method=\"post\" action=\"?action=sign-in\" {$hideConnection}>
-                    <h2>Se connecter</h2>
-                    <p>Email</p>
-                    <input type=\"email\" name=\"email\" placeholder=\"Email\">
-                    <p>Mot de passe</p>
-                    <input type=\"password\" name=\"password\" placeholder=\"Mot de passe\">
-                    <button type=\"submit\">Connexion</button>
-                    <a href=\"?action=register\">Créé un compte</a>
-                </form>
+        if($_GET['action'] == 'looktouite') {
+            $html = "<!DOCTYPE html>
+            <html lang=\"fr\">
+            <head>
+                <title>Touiteur</title>
+                <meta charset=\"utf-8\">
+                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+                <link rel=\"stylesheet\" href=\"css/mainStyle.css\">
+            </head>
+            
+            <body>
+                <header>
+                    <img class=\"logo\" src=\"./ressource/Logo.png\" alt=\"Le logo de touiteur\">
+                </header>
+            
+                <nav class=\"nav\">
+                    <a class=\"nav\" href=\"?\"><button class=\"stylized\">home</button></a>
+                    <a class=\"nav\" href=\"?action={$profil}\"><button class=\"stylized\">profile</button></a>
+                    <a class=\"nav\" href=\"?action=search\"><button class=\"stylized\">search</button></a>
+                </nav>
+            
+                <aside class=\"mascotte\">
+                    <img src=\"https://media.tenor.com/BXQgJskV7LgAAAAj/9999.gif\">
+                </aside>
                 ".$page."
-            </div>
-           </body>
-        <html>";
+            </body>
+            <html>";
 
-        echo $html;
+            echo $html;
+        }
+
+        else {
+            $html = "<!DOCTYPE html>
+            <html lang=\"fr\">
+            <head>
+                <title>Touiteur</title>
+                <meta charset=\"utf-8\">
+                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+                <link rel=\"stylesheet\" href=\"css/mainStyle.css\">
+            </head>
+            
+            <body>
+                <header>
+                    <img class=\"logo\" src=\"./ressource/Logo.png\" alt=\"Le logo de touiteur\">
+                </header>
+            
+                <nav class=\"nav\">
+                    <a class=\"nav\" href=\"?\"><button class=\"stylized\">home</button></a>
+                    <a class=\"nav\" href=\"?action={$profil}\"><button class=\"stylized\">profile</button></a>
+                    <a class=\"nav\" href=\"?action=search\"><button class=\"stylized\">search</button></a>
+                </nav>
+            
+                <aside class=\"mascotte\">
+                    <img src=\"https://media.tenor.com/BXQgJskV7LgAAAAj/9999.gif\">
+                </aside>
+            
+                <div id=\"feed\">
+                    <!-- NOTE : only visible for connected user-->
+                    <form class=\"post\" method=\"post\" action=\"?action=touite\" {$hidePost}>
+                        <h2>Envoyer un touite</h2>
+                        <textarea name=\"text\" maxlength=\"235\" placeholder=\"Votre incroyable touite ici\"> </textarea>
+                        <input type=\"file\" name=\"img\">
+                        <input type=\"text\" name=\"img_desc\" placeholder=\"Une description de votre image\">
+                        <button type=\"submit\">Envoyer le touite</button>
+                    </form>
+            
+                    <!-- when not connected, show that instead -->
+                    <form class=\"connection\" method=\"post\" action=\"?action=sign-in\" {$hideConnection}>
+                        <h2>Se connecter</h2>
+                        <p>Email</p>
+                        <input type=\"email\" name=\"email\" placeholder=\"Email\">
+                        <p>Mot de passe</p>
+                        <input type=\"password\" name=\"password\" placeholder=\"Mot de passe\">
+                        <button type=\"submit\">Connexion</button>
+                        <a href=\"?action=register\">Créé un compte</a>
+                    </form>
+                    ".$page."
+                </div>
+            </body>
+            <html>";
+
+            echo $html;
+        }
     }
 }
