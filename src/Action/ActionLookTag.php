394acefbf703567d3ad4f8e6608946f63ac72589
Flyguss <crafteur55100-verdun@hotmail.com>
2023-11-08 15:29:17 +0100

    codage de execute() de ActionLookTag et ActionLookUser

--- src/Action/ActionLookTag.php
+++ src/Action/ActionLookTag.php
@@ -2,10 +2,23 @@
 
 namespace touiteur\action;
 
+use touiteur\Tag\Tag;
+
 class ActionLookTag extends Action {
     public function execute() : string {
-        $_GET['idtag']; //the id of the user we want to look at
-        $html='Page de la personne avec ces touites les plus récents (TO DO)';
-        return $html;
+        {
+            $html = "<div id=\"followable\">
+            <h1>Tag</h1>
+
+            <!-- when the user is not connected, hide -->
+            <a class=\"tag\" href=\"htmlTemplate_TAGPAGE.html?action=followtag&idtag=".$_GET['idtag']."\"><button class=\"stylized\">Suivre</button></a>
+            <!-- when the user is following this tag, we change the button to this-->
+            <a class=\"tag\" href=\"htmlTemplate_TAGPAGE.html?action=unfollowtag&idtag=".$_GET['idtag']."\"><button class=\"stylized\">Ne plus suivre</button></a>
+        </div>";
+            $tag = Tag::getTagFromId($_GET['idtag']);
+            $touitList = $tag->getTouiteListFromTag();
+            $html .= $touitList->displayAllTouites() ;
+            return $html;
+        }
     }
 }
--- src/Action/ActionLookUser.php
+++ src/Action/ActionLookUser.php
@@ -2,10 +2,23 @@
 
 namespace touiteur\action;
 
+use touiteur\User\User;
+
 class ActionLookUser extends Action {
     public function execute() : string {
-        $_GET['iduser']; //the id of the user we want to look at
-        $html='Page de la personne avec ces touites les plus récents (TO DO)';
+        ; //the id of the user we want to look at
+        $html="<div id=\"followable\">
+            <h1></h1>
+
+            <!-- when the user is not connected, hide -->
+            <a class=\"user\" href=\"htmlTemplate_USERPAGE.html?action=followuser&iduser=".$_GET['iduser']."\"><button class=\"stylized\">Suivre</button></a>
+            <!-- when the user is following this user, we change the button to this-->
+            <a class=\"user\" href=\"htmlTemplate_USERPAGE.html?action=unfollowuser&iduser=".$_GET['iduser']."\"><button class=\"stylized\">Ne plus suivre</button></a>
+        </div>";
+
+        $user = User::getUserFromId($_GET['iduser']);
+        $touitList = $user->getTouit();
+        $html .= $touitList->displayAllTouites() ;
         return $html;
     }
 }
