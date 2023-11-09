6ee0feaa7e94f4f8c1451520f46533564543ec79
unknown <fouquettom2004@gmail.com>
2023-11-09 09:59:58 +0100

    Reglage ActionSignin

--- src/Action/ActionSignin.php
+++ src/Action/ActionSignin.php
@@ -14,7 +14,19 @@ class ActionSignin extends Action {
             $acttion=new ActionDisplayPage();
             $html=$acttion->execute();
         }
-        else{$html ='else we show the register page';}
+        else{$html ='<form class="connection" method="post" action="?action=register">
+            <h2>S\'inscrire</h2>
+            <p>Prénom</p>
+            <input type="text" name="name" placeholder="Prénom">
+            <p>Nom</p>
+            <input type="text" name="fullname" placeholder="Nom">
+            <p>Email</p>
+            <input type="email" name="mail" placeholder="Email">
+            <p>Mot de passe</p>
+            <input type="password" name="password" placeholder="Mot de passe">
+            <button type="submit">Connexion</button>
+            <p>Etes vous sur d\'avoir un compte ?</p>
+        </form>';}
         //register the person into the session
         return $html;
     }
