9292f8dfff7d99cbe1ae90952d6be9f971942e3f
unknown <fouquettom2004@gmail.com>
2023-11-09 11:09:43 +0100

    Reglage ActionVote

--- src/Action/ActionVote.php
+++ src/Action/ActionVote.php
@@ -9,10 +9,11 @@ class ActionVote extends Action {
     public function execute() : string {
         $touite=Touite::getTouiteFromId($_GET['idtouite']); //the id of the tag we want to look vote to
         $bool=$_GET['value']; //True if positive vote, false if negative vote
-        $user=User::getUserFromId($_SESSION['iduser']);
+        $user=($_SESSION['user']);
         $touite->evaluateTouite($user,$bool);
         //make the changes of the db
         $_GET['action'] = "";
-        return new ActionDisplayPage()->execute();
+        $action= new ActionDisplayPage();
+        return $action->execute();
     }
 }
