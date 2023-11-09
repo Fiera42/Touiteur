8dc67c9d95b7575b29358a0dde1f65ce51042c09
unknown <fouquettom2004@gmail.com>
2023-11-09 09:38:39 +0100

    Reglage ActionRegister

--- src/Action/ActionRegister.php
+++ src/Action/ActionRegister.php
@@ -1,22 +1,39 @@
 <?php
 
-namespace touiteur\action;
+namespace touiteur\Action;
 
 use touiteur\Auth\Auth;
 
 class ActionRegister extends Action {
-    public function execute() : string {
-        $name=$_POST['name']; //the name of the register user
-        $fullllllllnammmeeeeeee=$_POST['fullname']; //the fullname of the registering user
-        $mail=$_POST['email']; //the email of the registering user
-        $passwd=$_POST['password']; //the password of the registering user
-        if(Auth::register($mail,passwd,1,$name,$fullllllllnammmeeeeeee))
-        {
-            $acttion=new ActionDisplayPage();
-            $html=$acttion->execute();
-        }else {
-            $html ='else we show the register page (compte deja existant)';
-        }
-        return $html;
+    public function execute() : string
+    {
+        if ($_SERVER['REQUEST_METHOD']==='GET') {
+            $html = '<form class="connection" method="post" action="?action=register">
+            <h2>S\'inscrire</h2>
+            <p>Prénom</p>
+            <input type="text" name="name" placeholder="Prénom">
+            <p>Nom</p>
+            <input type="text" name="fullname" placeholder="Nom">
+            <p>Email</p>
+            <input type="email" name="mail" placeholder="Email">
+            <p>Mot de passe</p>
+            <input type="password" name="password" placeholder="Mot de passe">
+            <button type="submit">Connexion</button>
+        </form>';
+        } else if ($_SERVER['REQUEST_METHOD']==='POST') {
+            $name = $_POST['name']; //the name of the register user
+            $fullllllllnammmeeeeeee = $_POST['fullname']; //the fullname of the registering user
+            $mail = $_POST['mail']; //the email of the registering user
+            $passwd = $_POST['password']; //the password of the registering user
+            if (Auth::register($mail, $passwd, 1, $name, $fullllllllnammmeeeeeee)) {
+                $action = new ActionDisplayPage();
+                $html = $action->execute();
+            } else {
+                $html = 'else we show the register page (compte deja existant)';
+            }
+            return $html;
+        }return $html;
+
     }
+
 }
