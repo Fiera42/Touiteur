4f30adeb5eff786c93cb04ef0b43f87d92fdde57
unknown <fouquettom2004@gmail.com>
2023-11-10 15:38:45 +0100

    Ajout de l-action search

--- src/Action/ActionSearch.php
+++ src/Action/ActionSearch.php
@@ -1,21 +1,40 @@
 <?php
 
-namespace touiteur\action;
+namespace touiteur\Action;
 
 use touiteur\action\ActionLookUser ;
+use touiteur\auth\ConnexionFactory;
+use touiteur\TouiteList\TouiteList;
+use touiteur\User\Touite;
 
 class ActionSearch extends Action {
     public function execute() : string {
 
-        if($this->http_method === "GET") {
+        if ($_SERVER['REQUEST_METHOD']==='GET') {
             $html = "<form class=\"connection\" method=\"post\" action=\"?action=search\">
                     <h2>Rechercher</h2>
                     <input type=\"text\" name=\"search\" placeholder=\"Recherche\">
                     <button type=\"submit\">Rechercher</button>
                 </form>";
-            return $html ;
         }
+        else
+        {
+            $tag =$_POST['search'];
+            ConnexionFactory::makeConnection();
+            $pdo = ConnexionFactory::$db;
+            $query="Select idTouit from touittag natural join tag where tagname=?";
+            $prepared = $pdo->prepare($query);
+            $prepared->bindParam(1,$tag);
+            $list=new TouiteList();
+            $prepared->execute();
+            while($donne=$prepared->fetch())
+            {
+                $id = Touite::getTouiteFromId($donne['idTouit']);
+                $list->addTouite($id);
+            }
+            $html = $list->displayAllTouites();
 
-        else return "search result";
+        }
+        return $html;
     }
 }
