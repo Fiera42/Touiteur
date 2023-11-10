4a029f6c44054a80eec9e12226be68e274c91feb
Fiera42 <anaigeon@gmx.fr>
2023-11-10 09:57:19 +0100

    required forms

--- src/Action/ActionRegister.php
+++ src/Action/ActionRegister.php
@@ -21,8 +21,8 @@ class ActionRegister extends Action {
             <button type="submit">Connexion</button>
         </form>';
         } else if ($_SERVER['REQUEST_METHOD']==='POST') {
-            $name = filter_var($_POST['name'],FILTER_SANITIZE_STRING);        
-            $fullllllllnammmeeeeeee = filter_var($_POST['fullname'],FILTER_SANITIZE_STRING); 
+            $name = htmlentities($_POST['name']);        
+            $fullllllllnammmeeeeeee = htmlentities($_POST['fullname']); 
             $mail =  filter_var($_POST['mail'],FILTER_SANITIZE_EMAIL);                     
             $passwd = ($_POST['password']);                                                       
             if (Auth::register($mail, $passwd, $name, $fullllllllnammmeeeeeee)) {
@@ -32,13 +32,13 @@ class ActionRegister extends Action {
                 $html = '<form class="connection" method="post" action="?action=register">
             <h2>S\'inscrire</h2>
             <p>Prénom</p>
-            <input type="text" name="name" placeholder="Prénom">
+            <input type="text" name="name" placeholder="Prénom" required>
             <p>Nom</p>
-            <input type="text" name="fullname" placeholder="Nom">
+            <input type="text" name="fullname" placeholder="Nom" required>
             <p>Email</p>
-            <input type="email" name="mail" placeholder="Email">
+            <input type="email" name="mail" placeholder="Email" required>
             <p>Mot de passe</p>
-            <input type="password" name="password" placeholder="Mot de passe">
+            <input type="password" name="password" placeholder="Mot de passe" required>
             <button type="submit">Connexion</button>
             <p>Votre compte existe déjà</p>
         </form>';
